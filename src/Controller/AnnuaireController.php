e53343213a2abe8be1e4f72c5701a91413615450
Lucille Malozon <l.malozon@gmail.com>
2024-07-11 10:25:19 +0200

    route annuaire et test data sans bdd

---
+++ src/Controller/AnnuaireController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AnnuaireController extends AbstractController
+{
+    #[Route('/', name: 'app_annuaire')]
+    public function index(): Response
+    {
+        $contacts = [
+            [
+                'nom' => 'Dupont',
+                'prenom' => 'Jean',
+                'telephone' => '0123456789'
+            ],
+            [
+                'nom' => 'Durand',
+                'prenom' => 'Marie',
+                'telephone' => '0123456789'
+            ],
+            [
+                'nom' => 'Bouhadana',
+                'prenom' => 'Mohammed',
+                'telephone' => '0123456789'
+            ],
+        ];
+        return $this->render('annuaire/index.html.twig', [
+            'contacts' => $contacts,
+        ]);
+    }
+}
